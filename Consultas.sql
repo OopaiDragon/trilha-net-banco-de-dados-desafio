f18e4436ef8b4e8c7957e39ba57a10c2a68b5613
OopaiDragon <101565149+OopaiDragon@users.noreply.github.com>
2023-11-12 23:29:28 -0300

    Add files via upload

---
+++ Consultas.sql
@@ -0,0 +1,60 @@
+--1
+SELECT Nome, Ano FROM Filmes
+
+--2
+SELECT Nome, Ano, Duracao FROM Filmes ORDER BY Ano;
+
+--3
+SELECT Nome, Ano, Duracao from Filmes WHERE Nome = 'De Volta para o Futuro';
+
+--4
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Ano = 1997;
+
+--5
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Ano > 2000;
+
+--6
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Duracao > 100 AND Duracao < 150 ORDER BY Duracao;
+
+--7
+SELECT Ano, COUNT(*) AS quantidade FROM Filmes GROUP BY Ano ORDER BY quantidade DESC ;
+
+--8
+SELECT * FROM Atores WHERE Genero = 'M';
+
+--9
+SELECT * FROM Atores WHERE Genero = 'F' ORDER BY PrimeiroNome;
+
+--10
+SELECT 
+    Filmes.Nome, Generos.Genero 
+FROM 
+    FilmesGenero
+INNER JOIN 
+    Filmes ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN 
+    Generos ON Generos.Id = FilmesGenero.IdGenero;
+
+
+--11
+SELECT 
+    Filmes.Nome, Generos.Genero 
+FROM 
+    FilmesGenero
+INNER JOIN 
+    Filmes ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN 
+    Generos ON Generos.Id = FilmesGenero.IdGenero
+WHERE 
+    Generos.Genero = 'Mistério';
+
+
+--12
+SELECT 
+    Filmes.Nome, Atores.PrimeiroNome, Atores.UltimoNome, ElencoFilme.Papel 
+FROM 
+    ElencoFilme
+INNER JOIN 
+    Filmes ON Filmes.Id = ElencoFilme.IdFilme
+INNER JOIN 
+    Atores ON Atores.Id = ElencoFilme.IdAtor;
